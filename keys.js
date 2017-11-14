6c3bc55c0156141a73fdf0ccb5c4dbd71194773b
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-14 12:45:06 -0500

    Initial

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log("this is loaded");
+
+var twitterKeys = {
+  consumer_key: 'z1xL490fFk1ArQS9OkP6MvXx1',
+  consumer_secret: 'dMO1ci81QGRA9cjAwJRt7GMU8WiFo58WX8rJUD6bHV4lxKUZdK',
+  access_token_key: '930438309003788288-8rlzksbDMdig806IjIwZSEOgAfqEkyo',
+  access_token_secret: 'mmXKrhtqpg6qNmbD8YO3lvr99HA3DRiNxvCwsDpDLVXKp',
+}
+
+module.exports = twitterKeys;
