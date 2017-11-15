24a70e5afb62bd140b2466b6ab47400d93b2d872
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-15 11:54:10 -0500

    working

---
+++ liri.js
@@ -0,0 +1,176 @@
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require("request");
+var inquirer = require("inquirer");
+var keys = require("./keys.js");
+var fs = require("fs");
+
+var cmd = process.argv[2];
+// First step
+if (cmd) {
+  // if there is a command, do this...
+  switch (cmd) {
+    case "my-tweets":
+      bringTweets();
+      break;
+    case "spotify-this-song":
+      var input = "";
+      if (process.argv[3] === undefined) {
+        input = "TheSignAceofBase"
+      } else {
+        for (var i = 3; i < process.argv.length; i++) {
+          input = input + process.argv[i];
+        };
+      };
+      song(input);
+      break;
+    case "movie-this":
+      var input = "";
+      if (process.argv[3] === undefined) {
+        input = "Mr.Nobody";
+      } else {
+        for (var i = 3; i < process.argv.length; i++) {
+          input = input + process.argv[i];
+        };
+      };
+      findMovie(input);
+      break;
+    case "do-what-it-says":
+      fs.readFile("./random.txt", "utf8", function(err, data) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+          var random = data.replace(",", " ");
+          var final = random.split(" ");
+          console.log(final);
+        };
+      });
+      break;
+  };
+} else {
+  // if command  isn't inputed, use the GUI
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What is it you'd like to do today?",
+        choices: ["Show me tweets", "I want to know more about a song", "I want to know more about a movie", "Just do something, ANYTHING!"],
+        name: "firstStep"
+      }
+    ])
+    .then(function(inquirerResponse) {
+
+      var firstChoice = inquirerResponse.firstStep;
+
+      switch (firstChoice) {
+        case "Show me tweets":
+          bringTweets();
+          break;
+        case "I want to know more about a song":
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What song would you like to lookup?",
+                name: "song"
+              }
+            ])
+            .then(function(inquirerResponse) {
+              var search = inquirerResponse.song
+              song(search);
+            });
+          break;
+        case "I want to know more about a movie":
+          console.log("movie");
+          inquirer
+            .prompt([
+              {
+                type: "input",
+                message: "What movie would you like to search today?",
+                name: "movie"
+              }
+            ])
+            .then(function(inquirerResponse) {
+              var movie;
+              if (inquirerResponse.movie) {
+                movie = inquirerResponse.movie;
+              } else {
+                movie = "Mr.Nobody";
+              }
+
+              findMovie(movie.trim());
+            });
+          break;
+        case "Just do something, ANYTHING!":
+          console.log("random");
+          break;
+      };
+    });
+}
+
+
+// Twitter needs a lot of work
+function bringTweets() {
+  var client = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
+  });
+
+  var params = {screen_name: "@JimbobAlbob"};
+  client.get('statues/user_timeline', params, function(error, tweets, response) {
+     if (!error) {
+       console.log(tweets);
+     } else {
+       console.log(error);
+     };
+ });
+}
+
+function song(q) {
+  var spotify = new Spotify({
+    id: keys.spotifyKeys.clientID,
+    secret: keys.spotifyKeys.clientSecret
+  });
+
+  spotify.search({ type: 'track,artist', query: q, limit: '1' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    var spot = data.tracks.items[0];
+    console.log("");
+    console.log("");
+    console.log("Artist: " + spot.album.artists[0].name);
+    console.log("Song: " + spot.name);
+    console.log("On Spotify: " + spot.external_urls.spotify);
+    console.log("Album: " + spot.album.name);
+  });
+}
+
+function findMovie(mov) {
+  var base_url = 'http://www.omdbapi.com/?apikey=40e9cece&t=';
+
+  var queryUrl = base_url + mov;
+
+  request.get(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var bod = JSON.parse(body);
+      console.log("");
+      console.log("");
+      console.log("Title: " + bod.Title);
+      console.log("Release Year: " + bod.Year);
+      if (bod.Ratings[0] !== undefined) {
+        console.log("IMDB: " + bod.Ratings[0].Value);
+      };
+      if (bod.Ratings[1] !== undefined) {
+        console.log("Rotten Tomatoes: " + bod.Ratings[1].Value);
+      };
+      console.log("Country: " + bod.Country);
+      console.log("Language(s): " + bod.Language);
+      console.log("Plot Summary: " + bod.Plot);
+      console.log("Main Cast: " + bod.Actors);
+    };
+  });
+}
